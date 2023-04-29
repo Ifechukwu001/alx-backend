3bc7cd3950a79d40a0ae5185eac164eafeba2038
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-29 04:19:28 +0100

    Add configuration class and Babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class for flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+babel = Babel(app)
+
+
+@app.route("/")
+def hello():
+    """Renders index page"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
